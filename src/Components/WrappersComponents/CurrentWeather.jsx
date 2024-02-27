a9ef2043486a61cb8b90bb8ef5be837ccf916223
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-02-27 14:34:27 -0500

    TESTING IMAGE SELECTOR LOGIC

--- src/Components/WrappersComponents/CurrentWeather.jsx
+++ src/Components/WrappersComponents/CurrentWeather.jsx
@@ -6,23 +6,48 @@ import { Contex } from '../Hooks/Contex/CreateContext';
 //styles
 import '../../Sass/Components/WrappersComponents/CurrentWeather.scss';
 
+//images 
+import sun from '../../Assets/sun.png';
+import brokenClouds from '../../Assets/dark-cloud.png';
+import cloudy from '../../Assets/cloudy.png';
+
 export const CurrentWeather = () => {
 
     //contex
     const { weatherArrayInfo, GetCelcius } = useContext( Contex );
 
-    console.log('weatherArray is', GetCelcius)
+    //console.log('weatherArray is', GetCelcius)
+
+    let imageWeather;
+
+    if( weatherArrayInfo.weatherCondition  === 'light rain'){
+        imageWeather = sun;
+        
+        console.log( 'light rain' )
+   
+        
+    }else if ( weatherArrayInfo.weatherCondition  === 'broken clouds' ){
+        imageWeather = brokenClouds;
+
+    }else if ( weatherArrayInfo.weatherCondition  === 'overcast clouds' ){
+        imageWeather = cloudy;
+    }
      
+
    return ( 
         <section className="currentWeatherWrapper">
  
-          {/*   <figure className="currentWeatherImage"> 
-                <img alt="image_weather_state"></img> 
-            </figure> */}
+            <figure className="currentWeatherImage"> 
+                { 
+                    
+                    imageWeather && <img src={ imageWeather  } alt="image status weather" ></img>
+                    
+                }
+            </figure> 
 
             <section className="currentWeatherInfo">
 
-                <h3 className="weatherCondition" > { weatherArrayInfo.weatherCondition} </h3>
+                <h3 className="weatherCondition" > { weatherArrayInfo.weatherCondition } </h3>
                
                 <h3 className="temperature"> { GetCelcius }º C  </h3>
                
